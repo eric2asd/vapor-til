5ff5c79e55cee286134287a29b51048125042bff
陳信毅 <>
2018-08-22 21:48:10 +0800

    Add sibling relationships

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -69,6 +69,8 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     // migrations.add(model: Acronym.self, database: .mysql)
     migrations.add(model: User.self, database: .psql)
     migrations.add(model: Acronym.self, database: .psql)
+    migrations.add(model: Category.self, database: .psql)
+    migrations.add(model: AcronymCategoryPivot.self, database: .psql)
     services.register(migrations)
     
     var commandConfig = CommandConfig.default()
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -13,4 +13,8 @@ public func routes(_ router: Router) throws {
 
     let usersController = UsersController()
     try router.register(collection: usersController)
+    
+    let categoriesController = CategoriesController()
+    try router.register(collection: categoriesController)
+
 }
